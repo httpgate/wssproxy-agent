f81ed684573aaca1d2f422a56bcf8ee6d160276c
http <xinjingjie.us@gmail.com>
2023-01-29 12:07:49 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# wssproxy-agent
+A local proxy agent connecting to remote websocet proxy server
+
+把远端的websocket加密代理服务器映射为本地的非加密普通代理服务器
+
+采用DOH(DNS over https)保护用户隐私
+
+# 运行
+
+可下载直接点击[绿色可执行文件](https://github.com/httpgate/resouces/tree/main/wssproxy-agent)，或加上可选命令行参数。默认只本机能用代理，加-s可分享本机IP和端口给同一网段：
+
+node ./wssagent.js [wssurl] [proxy-port] [-s] [DOH-Url]
+
+./wssagent-linux [wssurl] [proxy-port] [-s] [DOH-Url]
+
+Linux系统下的可执行文件只能在命令行下执行，除了wssurl外其它参数不是必须输入
+
+# 使用
+
+获得wssurl, 和"pac url"的区别是把 https 改成 wss 
+
+运行wssagent
+
+浏览器设置代理服务器为 localhost 代理端口为 wssagent 显示的端口
+
+
+# 用途
+
+很多软件不支持https加密的pac代理， 用wssagent就可以在一台电脑上把加密代理变成普通代理，整个局域网都可以按普通方式代理上网
+
+如果海外的pacproxy服务器被封了，这有可能是IP被封了，或者域名被封了。这时你可以自己注册一个域名，在cloudflare之类的支持websocket的CDN上注册一个账户， 再在CDN上把这个域名指向你远端的pacproxy服务器，这时就又可以上网了。
+
+这时域名变成了你注册的域名，但域名后面的url还是原来的pacproxy的url, 就可以连上了。enjoy and, 法轮大法好，真善忍好。
---
+++ wssagent.js
@@ -0,0 +1,95 @@
+#!/usr/bin/env node
+
+//wss url like wss://site.domain/url
+var wssurl = '';
+//proxy server listening port，same as port in firefox proxy
+var proxyport = 0 ;
+var shareproxy = false;
+var dohUrl = 'https://mozilla.cloudflare-dns.com/dns-query';
+
+const net = require('net');
+const WebSocket = require('ws');
+const readline = require('readline-sync');
+const dns = require('dns');
+const { Dohnut } = require('dohnut');
+const dnsServer = '127.0.0.1:51392';
+
+
+function run(configs){
+  if(configs) {
+    wssurl = configs.url;
+    proxyport = configs.proxyport;
+    shareproxy = configs.shareproxy;
+    dohUrl = configs.dohUrl;    
+  }
+  else if(process.argv[2]){
+    wssurl = process.argv[2];
+
+    if(process.argv[3] && !isNaN(process.argv[3])) proxyport = process.argv[3];
+    
+    if(process.argv[4] && (process.argv[4].toLowerCase()=='-s')) shareproxy = true;
+
+    if(process.argv[5] && (process.argv[5].toLowerCase().startsWith('https://'))) dohUrl = process.argv[5];
+
+  } else {
+    wssurl = readline.question('\r\nInput websocket wss url:');
+    if((!wssurl) || (!wssurl.toLowerCase().startsWith('wss://'))) return readline.question('\r\nivalid websocket wss url[ok]');
+
+    let inport = readline.question('\r\nInput proxy port [Random]:');
+    if(inport && (!isNaN(inport))) proxyport = inport;
+
+    let inshare = readline.question('\r\nShare proxy with others? [No]:');
+    if(inshare && (inshare.toLowerCase().startsWith('y'))) shareproxy = true;
+
+    let indoh = readline.question('\r\ninput DOH provider ['+dohUrl +']:\r\n');
+    if(indoh && (indoh.toLowerCase().startsWith('https://'))) dohUrl = indoh;
+  }
+
+  var configuration = {
+    dns: [{ type: 'udp4', addres: '127.0.0.1', port: 51392 }],
+    doh: [{ uri: dohUrl }],
+    bootstrap : '1.1.1.1',
+    countermeasures : ''
+  }
+  const dohnut = new Dohnut(configuration)
+  start();
+}
+
+
+async function start() {
+  await dohnut.start()
+  dns.setServers([dnsServer]);
+
+  const server = net.createServer(c => {
+      const ws = new WebSocket(wssurl)
+
+      ws.on('close', () => c.destroy())
+
+      ws.on('error', () => c.destroy())
+
+      c.on('end', () => ws.close(1000))
+
+      c.on('error', () => ws.close(1000))
+
+      ws.on('open', () => c.on('data', data => ws.send(data)))
+
+      ws.on('message', data => {
+        if (!c.destroyed)
+          c.write(data)
+      })
+  })
+
+  server.on('error', (err) => {
+    console.log('error');
+  })
+
+  if(shareproxy){
+    server.listen(proxyport, ()=>console.log('\r\nwssagent serve in port : ' + server.address().port));
+  } else {
+    server.listen(proxyport, '127.0.0.1', ()=>console.log('\r\nwssagent serve in port : ' + server.address().port));
+  }
+}
+
+if(process.argv[1].includes(__filename)) run();
+
+exports.run = run;

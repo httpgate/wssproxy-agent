a94e5763fb8c8cfb7409c242cf105dbb64752d0e
http <xinjingjie.us@gmail.com>
2023-02-28 12:46:15 -0500

    document and comments update

--- run-in-container/wssagent.js
+++ run-in-container/wssagent.js
@@ -14,10 +14,12 @@ var configs =
     //代理服务器IP地址, 格式：'xxx.xxx.xxx.xxx', 多个IP用英文小写逗号 , 隔开。指定IP可绕开DNS解析，避免域名劫持或DNS封锁
         wssip : '',
 
-    //连接域名，连接代理服务器时此域名会替换掉 wssurl 里的域名，以避开域名审查，隐藏真实域名
+    //连接域名，连接代理服务器时此域名会替换掉 wssurl 里的域名，以避开域名审查，隐藏真实域名，不适用于CDN, 仅在wssip是直连IP时有用
         connectDomain : '',
         
-    //DOH(DNS Over Https)服务器域名, 如"dns.cloudflare.com", "doh.opendns.com", "dns.google"
+    //DOH(DNS Over Https)服务器域名, 如 'dns.cloudflare.com', 'doh.opendns.com', 'dns.google'
+    //新版本也支持DOH URL, 如： 'https://cdn.nextcdn.io/youraccout'
+
         dohServer : 'mozilla.cloudflare-dns.com'
 
 }
